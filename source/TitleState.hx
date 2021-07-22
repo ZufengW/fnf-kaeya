94de7650d6cd0f0d0e79128d5e34e2d80a2e3ca1
Zufeng Wang <zufengw@gmail.com>
2021-07-22 19:34:11 +1000

    Disable unneeded pages
    Pages removed:
    * Donate
    * Options (because it looks broken)
    * Old version
    
    And remove trailing whitespace in those files.

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -290,21 +290,21 @@ class TitleState extends MusicBeatState
 			{
 				// Check if version is outdated
 
-				var version:String = "v" + Application.current.meta.get('version');
+				// var version:String = "v" + Application.current.meta.get('version');
 
-				if (version.trim() != NGio.GAME_VER_NUMS.trim() && !OutdatedSubState.leftState)
-				{
-					FlxG.switchState(new OutdatedSubState());
-					trace('OLD VERSION!');
-					trace('old ver');
-					trace(version.trim());
-					trace('cur ver');
-					trace(NGio.GAME_VER_NUMS.trim());
-				}
-				else
-				{
-					FlxG.switchState(new MainMenuState());
-				}
+				// if (version.trim() != NGio.GAME_VER_NUMS.trim() && !OutdatedSubState.leftState)
+				// {
+				// 	FlxG.switchState(new OutdatedSubState());
+				// 	trace('OLD VERSION!');
+				// 	trace('old ver');
+				// 	trace(version.trim());
+				// 	trace('cur ver');
+				// 	trace(NGio.GAME_VER_NUMS.trim());
+				// }
+				// else
+				// {
+				FlxG.switchState(new MainMenuState());
+				// }
 			});
 			// FlxG.sound.play(Paths.music('titleShoot'), 0.7);
 		}
